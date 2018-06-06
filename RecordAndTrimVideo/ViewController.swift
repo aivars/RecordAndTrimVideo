2d1d325b525c692327fe9aeeb691567bdaf320ba
Aivars <aivars.meijers@gmail.com>
2018-06-06 14:24:03 +0300

    Initial Commit

---
+++ RecordAndTrimVideo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RecordAndTrimVideo
+//
+//  Created by Aivars Meijers on 06/06/2018.
+//  Copyright © 2018 Aivars Meijers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
