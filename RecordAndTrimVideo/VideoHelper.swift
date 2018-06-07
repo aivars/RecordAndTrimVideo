fb877f867aa27da545918c1b100c4c01546f2098
Aivars <aivars.meijers@gmail.com>
2018-06-07 09:43:08 +0300

    Selected video is limited for defined time

---
+++ RecordAndTrimVideo/VideoHelper.swift
@@ -0,0 +1,28 @@
+//
+//  VideoHelper.swift
+//  RecordAndTrimVideo
+//
+//  Created by Aivars Meijers on 06/06/2018.
+//  Copyright © 2018 Aivars Meijers. All rights reserved.
+//
+
+import UIKit
+import MobileCoreServices
+import AVFoundation
+
+class VideoHelper {
+    
+    static func startMediaBrowser(delegate: UIViewController & UINavigationControllerDelegate & UIImagePickerControllerDelegate, sourceType: UIImagePickerControllerSourceType) {
+        guard UIImagePickerController.isSourceTypeAvailable(sourceType) else { return }
+        
+        
+        
+        let mediaUI = UIImagePickerController()
+        mediaUI.sourceType = sourceType
+        mediaUI.mediaTypes = [kUTTypeMovie as String]
+        mediaUI.allowsEditing = true
+        mediaUI.delegate = delegate
+        delegate.present(mediaUI, animated: true, completion: nil)
+    }
+    
+}
--- RecordAndTrimVideo/ViewController.swift
+++ RecordAndTrimVideo/ViewController.swift
@@ -7,19 +7,98 @@
 //
 
 import UIKit
+import AVKit
+import MobileCoreServices
 
 class ViewController: UIViewController {
 
+  
+    
+    @IBAction func playVideo(_ sender: AnyObject) {
+        VideoHelper.startMediaBrowser(delegate: self, sourceType: .savedPhotosAlbum)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
+
+// MARK: - UIImagePickerControllerDelegate
+
+extension ViewController: UIImagePickerControllerDelegate {
+    
+    
+    func cropVideo(sourceURL: URL, startTime: Double, endTime: Double, completion: ((_ outputUrl: URL) -> Void)? = nil)
+    {
+        let fileManager = FileManager.default
+        let documentDirectory = fileManager.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        
+        let asset = AVAsset(url: sourceURL)
+        let length = Float(asset.duration.value) / Float(asset.duration.timescale)
+        print("video length: \(length) seconds")
+        
+        var outputURL = documentDirectory.appendingPathComponent("output")
+        do {
+            try fileManager.createDirectory(at: outputURL, withIntermediateDirectories: true, attributes: nil)
+            outputURL = outputURL.appendingPathComponent("\(sourceURL.lastPathComponent).mp4")
+        }catch let error {
+            print(error)
+        }
+        
+        //Remove existing file
+        try? fileManager.removeItem(at: outputURL)
+        
+        guard let exportSession = AVAssetExportSession(asset: asset, presetName: AVAssetExportPresetHighestQuality) else { return }
+        exportSession.outputURL = outputURL
+        exportSession.outputFileType = .mp4
+        
+        let timeRange = CMTimeRange(start: CMTime(seconds: startTime, preferredTimescale: 1000),
+                                    end: CMTime(seconds: endTime, preferredTimescale: 1000))
+        
+        exportSession.timeRange = timeRange
+        exportSession.exportAsynchronously {
+            switch exportSession.status {
+            case .completed:
+                print("exported at \(outputURL)")
+                completion?(outputURL)
+                
+                // play preview video
+                let player = AVPlayer(url: outputURL)
+                let vcPlayer = AVPlayerViewController()
+                vcPlayer.player = player
+                self.present(vcPlayer, animated: true, completion: nil)
+
+            case .failed:
+                print("failed \(exportSession.error.debugDescription)")
+            case .cancelled:
+                print("cancelled \(exportSession.error.debugDescription)")
+            default: break
+            }
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        guard let mediaType = info[UIImagePickerControllerMediaType] as? String,
+            mediaType == (kUTTypeMovie as String),
+            let mediaUrl = info[UIImagePickerControllerMediaURL] as? URL
+            else { return }
+        self.cropVideo(sourceURL: mediaUrl, startTime: 0.0, endTime: 15.00)
+        
+        dismiss(animated: true) {
+            
+            
+        }
+    }
+}
+
+
+
+// MARK: - UINavigationControllerDelegate
+
+extension ViewController: UINavigationControllerDelegate {
+}
+
 
